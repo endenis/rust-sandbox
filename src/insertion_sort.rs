a3107be2b35822af501d00ac61e2ba830d0398e8
endenis <endenis@gmail.com>
2015-03-28 17:06:48 +0100

    Update for latest Rust

--- src/insertion_sort.rs
+++ src/insertion_sort.rs
@@ -1,23 +1,23 @@
 // Based on gist https://gist.github.com/endenis/25b3b9e5cd3b3f002178
 // Two implementations of insertion sort.
 
-fn insertion_sort1(arr: &mut Vec<int>) -> &Vec<int> {
-    for i in range(1, arr.len()) {
+fn insertion_sort1(arr: &mut Vec<isize>) -> &Vec<isize> {
+    for i in 1..arr.len() {
         let key = arr[i];
-        let mut j = (i - 1) as int;
-        let mut j_u = j as uint;
+        let mut j = (i - 1) as isize;
+        let mut j_u = j as usize;
         while j >= 0 && arr[j_u] > key {
             arr[j_u + 1] = arr[j_u];
             j = j - 1;
-            j_u = j as uint;
+            j_u = j as usize;
         }
-        arr[j_u + 1] = key;
+        arr[(j + 1) as usize] = key;
     }
     arr
 }
 
-fn insertion_sort2(arr: &mut Vec<int>) -> &Vec<int> {
-    for i in range(1, arr.len()) {
+fn insertion_sort2(arr: &mut Vec<isize>) -> &Vec<isize> {
+    for i in 1..arr.len() {
         let mut j = i;
         while j > 0 && arr[j] < arr[j - 1] {
             let buf = arr[j];
@@ -32,11 +32,11 @@ fn insertion_sort2(arr: &mut Vec<int>) -> &Vec<int> {
 fn main() {
     let mut vector1 = vec![5, 8, 6, 3, 7, 0, 4, 1];
     let mut vector2 = vector1.clone();
-    println!("Original vector: {}", vector1);
+    println!("Original vector: {:?}", vector1);
 
     insertion_sort1(&mut vector1);
-    println!("Insertion sort1: {}", vector1);
+    println!("Insertion sort1: {:?}", vector1);
 
     insertion_sort2(&mut vector2);
-    println!("Insertion sort2: {}", vector2);
+    println!("Insertion sort2: {:?}", vector2);
 }
